e20c9ceb6795dd6f2cf74167809d531c70e4b9ce
Giangi99 <70216281+Giangi99@users.noreply.github.com>
2020-11-25 12:45:58 +0100

    Wrap code in a function get_weather

--- open_weather.py
+++ open_weather.py
@@ -1,12 +1,36 @@
 import requests
-import json
 
-api_key = "e8707e5c7eb2b3c36d45fe108cf994f9"
-lat = "45.467175"
-lon = "9.189664"
+'''
+This module is used in order to retrive the data from the OpenWeather service
+'''
+API_KEY = "e8707e5c7eb2b3c36d45fe108cf994f9"  # key needed to call OpenWeather apis
 
-url = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
+def get_weather(lat, lon):
+    """
+    Get weather information by position
+    :param lat: position latitude
+    :param lon: position longitude
+    :return: json with weather info
+    """
+    URL = "https://api.openweathermap.org/data/2.5/weather"  # api-endpoint
 
-response = requests.get(url)
-data = json.loads(response.text)
-print(data)
+    # defining a params dict for the parameters to be sent to the API
+    PARAMS = {
+        "lat": lat,
+        "lon": lon,
+        "appid": API_KEY,
+        "units": "metric"
+    }
+
+    # sending get request and saving the response as response object
+    response = requests.get(url=URL, params=PARAMS)
+
+    # extracting data in json format
+    data = response.json()
+
+    return {
+        "weather": data["weather"][0]["main"],
+        "description": data["weather"][0]["description"],
+        "icon": data["weather"][0]["icon"],
+        "temperature": data["main"]["temp"]
+    }
